9a17058461ac78daf911fcf305a9459b9147b388
Robert Davies <robert@theriftlab.com>
2021-12-17 17:28:22 -0800

    Spacing.

--- immanuel/serializable.py
+++ immanuel/serializable.py
@@ -20,11 +20,11 @@ class Serializable:
 
     def serialize(self) -> dict:
         """ Returns a dict of all public members. """
-        return {k:v.serialize() if isinstance(v, Serializable) else v for k, v in self._public_items()}
+        return {k: v.serialize() if isinstance(v, Serializable) else v for k, v in self._public_items()}
 
     def _public_items(self) -> dict:
         """ Returns all members not starting with '_'. """
-        return {k:v for k, v in self.__dict__.items() if k[0] != '_'}.items()
+        return {k: v for k, v in self.__dict__.items() if k[0] != '_'}.items()
 
     def __iter__(self):
         """ Simple iterator for public members. """
