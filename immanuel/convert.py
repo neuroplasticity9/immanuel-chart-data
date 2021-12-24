d3210c9f233b538ea01c59bf3febb2f7731a60d7
Robert Davies <robert@theriftlab.com>
2021-12-24 09:16:10 -0800

    Refined convert module.

--- immanuel/convert.py
+++ immanuel/convert.py
@@ -15,38 +15,31 @@ import math
 import re
 from decimal import Decimal
 
+import swisseph as swe
+
 
 FORMAT_TIME = 0
 FORMAT_DMS = 1
 FORMAT_LAT = 2
 FORMAT_LON = 3
 
+ROUND_DEGREE = (1, swe.SPLIT_DEG_ROUND_DEG)
+ROUND_MINUTE = (2, swe.SPLIT_DEG_ROUND_MIN)
+ROUND_SECOND = (3, swe.SPLIT_DEG_ROUND_SEC)
 
-def dms_to_dec(dms: list) -> float:
+
+def dms_to_dec(dms: list | tuple) -> float:
     """ Returns the decimal conversion of a D:M:S list. """
     dec = sum([float(v) / 60**k for k, v in enumerate(dms[1:])])
     return dec if dms[0] == '+' else -dec
 
 
-def dec_to_dms(dec: float) -> list[str | int]:
+def dec_to_dms(dec: float, round_to: tuple = ROUND_SECOND) -> tuple:
     """ Returns the rounded D:M:S conversion of a decimal float. """
-    dms = ['-' if dec < 0 else '+', abs(dec)]
-
-    for i in range(1, 3):
-        dms.append(Decimal(str(dms[i])) % 1 * 60)
-        dms[i] = int(dms[i])
-
-    dms[3] = round(dms[3])
-
-    for i in range(3, 1, -1):
-        if dms[i] == 60:
-            dms[i-1] += 1
-            dms[i] = 0
-
-    return dms
+    return ('-' if dec < 0 else '+', *swe.split_deg(dec, round_to[1])[:round_to[0]])
 
 
-def dms_to_string(dms: list, format: int = FORMAT_DMS) -> str:
+def dms_to_string(dms: list | tuple, format: int = FORMAT_DMS) -> str:
     """ Returns a D:M:S list as either a D:M:S, D°M'S" or
     coordinate string. """
     if format == FORMAT_DMS or format == FORMAT_TIME:
@@ -70,7 +63,7 @@ def dms_to_string(dms: list, format: int = FORMAT_DMS) -> str:
     return string
 
 
-def string_to_dms(string: str) -> list[str | int]:
+def string_to_dms(string: str) -> tuple:
     """ Takes either a float, a float-as-string, a 12w34.56-type string,
     or a 12°34'56.78" / 12:34:56.78-type string, and returns a float. """
     try:
@@ -81,9 +74,9 @@ def string_to_dms(string: str) -> list[str | int]:
         char = string[len(digits[0])].upper()
 
         if char in 'NESW':
-            return ['-' if char in 'SW' else '+'] + floats
+            return ('-' if char in 'SW' else '+', *floats)
         else:
-            return ['-' if floats[0] < 0 else '+', abs(floats[0])] + floats[1:]
+            return ('-' if floats[0] < 0 else '+', abs(floats[0]), *floats[1:])
 
 
 def dec_to_string(dec: float, format: int = FORMAT_DMS) -> str:
