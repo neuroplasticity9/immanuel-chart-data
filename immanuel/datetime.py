50574363e7ec4c0a59c0285ea15d8a828bb7ebc9
Robert Davies <robert@theriftlab.com>
2022-01-17 20:16:52 -0800

    Complete refactor of DateTime class for greater localisation accuracy.

--- immanuel/datetime.py
+++ immanuel/datetime.py
@@ -3,75 +3,83 @@
     Author: Robert Davies (robert@theriftlab.com)
 
 
-    This module provides a simple class for handling location-specific
-    dates and times.
+    This module provides a simple class and helper functions for converting
+    between location-specific Gregorian dates and times, and universal
+    Julian dates.
 
-    The class essentially wraps pyswisseph's jul_day() function but
-    additionally takes into account timezones based on lat/long coordinates
-    and does the heavy lifting for you.
+    The class and functions essentially wrap pyswisseph's jul_day() and
+    jdut1_to_utc() functions but take into account timezones based on
+    lat/long coordinates and do the heavy lifting for you.
 
 """
 
 from datetime import datetime
 
 import swisseph as swe
-from pytz import timezone, utc, exceptions
+from pytz import exceptions, timezone, UTC
 from timezonefinder import TimezoneFinder
 
 from immanuel import convert
 
 
 class DateTime:
-    """ This class is instatiated with a standard Python datetime object,
-    and decimal lat / lon coordinates as floats. is_dst can be True or False
-    to clarify ambiguous datetimes (eg. 01:30 when DST ends).
+    """ This class is instatiated with either a standard Python datetime
+    object or a Julian day as a float, and decimal lat / lon coordinates
+    as floats. is_dst can be True or False to clarify ambiguous datetimes
+    (eg. 01:30 when DST ends).
 
     """
 
-    def __init__(self, dt: datetime, lat: float, lon: float, is_dst = None):
-        self._dt = dt
-        self._lat = lat
-        self._lon = lon
-        self._is_dst = is_dst
-        self.dst_ambiguous = False
-        self.timezone = self._timezone()
-        self.offset = self._offset()
-        self.jd = None if self.offset is None else self._jd()
+    def __init__(self, dt_jd: datetime | float, lat: float, lon: float, is_dst: bool = None):
+        self.datetime = None
+        self.timezone = TimezoneFinder().certain_timezone_at(lat=lat, lng=lon)
+        self.dst_ambiguous = None
+        self.jd = None
 
-    @staticmethod
-    def from_jd(jd: float, lat: float, lon: float, is_dst = None):
-        utc = swe.jdut1_to_utc(jd)
-        seconds_float = utc[5]
-        seconds = int(seconds_float)
-        microseconds = round((seconds_float - seconds) * 1000)
-        dt_utc = utc[:5] + (seconds, microseconds)
-        dt = datetime(*dt_utc)
-        return DateTime(dt, lat, lon)
+        if (isinstance(dt_jd, datetime)):
+            self._dt_instance(dt_jd, is_dst)
+        else:
+            self._jd_instance(dt_jd)
 
-    def isoformat(self):
-        return f'{self._dt.isoformat()}{convert.dec_to_string(self.offset, convert.FORMAT_TIME_OFFSET, convert.ROUND_MINUTE)}'
-
-    def _timezone(self) -> str:
-        """ Returns the timezone's name. """
-        return TimezoneFinder().certain_timezone_at(lat=self._lat, lng=self._lon)
-
-    def _offset(self) -> float:
-        """ Returns the timezone's offset after DST ambiguity check. """
-        tz = timezone(self.timezone)
-
+    def _dt_instance(self, dt: datetime, is_dst: bool):
+        """ Accept a datetime object, localise it, and calculate the
+        universal Julian day. """
         try:
-            dt_local = tz.localize(self._dt, self._is_dst)
+            self.datetime = timezone(self.timezone).localize(dt, is_dst)
         except exceptions.AmbiguousTimeError:
-            self.dst_ambiguous = True
-            return None
+            self.datetime = None
 
-        dt_utc = utc.localize(self._dt)
-        return (dt_utc - dt_local).total_seconds() / 3600
+        self.jd = datetime_to_jd(self.datetime) if self.datetime is not None else None
+        self.dst_ambiguous = self.datetime is None
 
-    def _jd(self) -> float:
-        """ Returns the Julian date. """
-        hour = convert.dms_to_dec(['+', self._dt.hour-self.offset, self._dt.minute, self._dt.second])
-        return swe.julday(self._dt.year, self._dt.month, self._dt.day, hour)
+    def _jd_instance(self, jd: float):
+        """ Accept a universal Julian day and generate a localised
+        datetime object. """
+        self.jd = jd
+        self.datetime = jd_to_datetime(jd).astimezone(timezone(self.timezone))
+        self.dst_ambiguous = False
 
-    def __str__(self):
-        return f'{self._dt.strftime("%a %d %b %Y %H:%M:%S")} {self.timezone}'
+    def isoformat(self) -> str:
+        """ Returns the underlying datetime object's ISO format. """
+        return self.datetime.isoformat()
+
+    def __str__(self) -> str:
+        """ Returns the full date with timezone string. """
+        return f'{self.datetime.strftime("%a %d %b %Y %H:%M:%S")} {self.timezone}'
+
+
+def datetime_to_jd(dt: datetime) -> float:
+    """ Convert localised datetime into universal Julian day. """
+    dt = dt.astimezone(UTC)
+    hour = convert.dms_to_dec(['+', dt.hour, dt.minute, dt.second])
+    return swe.julday(dt.year, dt.month, dt.day, hour)
+
+
+def jd_to_datetime(jd: float) -> datetime:
+    """ Convert Julian day into UTC datetime object. """
+    swe_utc = swe.jdut1_to_utc(jd)
+    seconds_float = swe_utc[5]
+    seconds = int(seconds_float)
+    microseconds = round((seconds_float - seconds) * 1000)
+    utc = swe_utc[:5] + (seconds, microseconds)
+    return datetime(*utc, tzinfo=UTC)
